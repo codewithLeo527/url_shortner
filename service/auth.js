dd47325f56116ec2b782a28103c05f5a933edea4
SandeshNakkawar <sandeshnakkawar32@gmail.com>
2025-03-22 12:58:39 +0530

    Initial commit

---
+++ service/auth.js
@@ -0,0 +1,14 @@
+const sessionIdToUsermap = new Map();
+
+function setUser(id, user){
+    sessionIdToUsermap.set(id, user);
+}
+
+function getUser(id){
+    return sessionIdToUsermap.get(id);
+}
+
+module.exports = {
+    getUser,
+    setUser
+}
